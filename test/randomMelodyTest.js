e75e55770e01e2f406f0e63196900aa502644416
Jay Quigley <quigley.james.g@gmail.com>
2021-05-27 17:32:28 -0400

    fix test

--- test/randomMelodyTest.js
+++ test/randomMelodyTest.js
@@ -4,10 +4,10 @@ import randomMelody from '../src/options/randomMelody';
 describe("randomMelody", () => {
   describe("generating a random melody, given a key, a mode, and a measure length", () => {
     it("works for C major", () => {
-      const scaleNoteName = 'C';
+      const scaleRootNote = 'C';
       const mode = 'ionian';
       const beatsPerMeasure = 4;
-      expect(randomMelody({ scaleNoteName, mode, beatsPerMeasure }).join('')).to
+      expect(randomMelody({ scaleRootNote, mode, beatsPerMeasure }).join('')).to
         .match(/([ABCDEFG=_]{4}){4}/);
     });
   })
