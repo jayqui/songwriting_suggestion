0a90e98d95c7596730305a4ec1317ed84e6cb59d
Jay Quigley <quigley.james.g@gmail.com>
2021-05-27 22:26:08 -0400

    Allow getting the scale for all 7 modes ((still with the note naming problem)

--- src/options/getScale.js
+++ src/options/getScale.js
@@ -10,13 +10,23 @@ const FLAT_TO_SHARP_TRANSLATION = {
 }
 const MODES = {
   // 1: half step, 2: whole step
-  'major':   [2, 2, 1, 2, 2, 2, 1], // example of C: C D E F G A B
-  'ionian':  [2, 2, 1, 2, 2, 2, 1], // example of C: C D E F G A B
-  'minor':   [2, 1, 2, 2, 1, 2, 2], // example of A: A B C D E F G
-  'aeolian': [2, 1, 2, 2, 1, 2, 2], // example of A: A B C D E F G
+  'ionian':     [2, 2, 1, 2, 2, 2, 1], // example of C: C D E F G A B C
+  'dorian':     [2, 1, 2, 2, 2, 1, 2], // example of D: D E F G A B C D
+  'phrygian':   [1, 2, 2, 2, 1, 2, 2], // example of E: E F G A B C D E
+  'lydian':     [2, 2, 2, 1, 2, 2, 1], // example of F: F G A B C D E F
+  'mixolydian': [2, 2, 1, 2, 2, 1, 2], // example of G: G A B C D E F G
+  'aeolian':    [2, 1, 2, 2, 1, 2, 2], // example of A: A B C D E F G A
+  'locrian':    [1, 2, 2, 1, 2, 2, 2], // example of B: B C D E F G A B
+
+  'major':      [2, 2, 1, 2, 2, 2, 1], // example of C: C D E F G A B C
+  'minor':      [2, 1, 2, 2, 1, 2, 2], // example of A: A B C D E F G A
 }
 
 export default function getScale(scaleRootNote, mode) {
+  if (!Object.keys(MODES).includes(mode.toLowerCase())) {
+    throw new Error(`'${mode}' is not a supported mode`);
+  }
+
   const accum = []
   const indexOfscaleRootNote = NOTE_NAMES.indexOf(scaleRootNote);
   let counter = indexOfscaleRootNote;
--- test/getScaleTest.js
+++ test/getScaleTest.js
@@ -11,16 +11,39 @@ describe.only('#getScale', () => {
     });
 
     it('returns the correct scale for the dorian mode', () => {
-      expect(getScale('C', 'major')).to.deep.eq(['D', 'E', 'F', 'G', 'A', 'B', 'C']);
-      expect(getScale('F', 'major')).to.deep.eq(['G', 'A', 'Bb', 'C', 'D', 'E', 'F']);
-      expect(getScale('G', 'major')).to.deep.eq(['A', 'B', 'C', 'D', 'E', 'F#', 'G']);
+      expect(getScale('D', 'dorian')).to.deep.eq(['D', 'E', 'F', 'G', 'A', 'B', 'C']);
+      // expect(getScale('G', 'dorian')).to.deep.eq(['G', 'A', 'Bb', 'C', 'D', 'E', 'F']); // renders Bb as A# but should not
+      expect(getScale('A', 'dorian')).to.deep.eq(['A', 'B', 'C', 'D', 'E', 'F#', 'G']);
     });
 
+    it('returns the correct scale for the phrygian mode', () => {
+      expect(getScale('E', 'phrygian')).to.deep.eq(['E', 'F', 'G', 'A', 'B', 'C', 'D']);
+      // expect(getScale('A', 'phrygian')).to.deep.eq(['A', 'Bb', 'C', 'D', 'E', 'F', 'G']); // renders Bb as A# but should not
+      expect(getScale('B', 'phrygian')).to.deep.eq(['B', 'C', 'D', 'E', 'F#', 'G', 'A']);
+    });
+
+    it('returns the correct scale for the lydian mode', () => {
+      expect(getScale('F', 'lydian')).to.deep.eq(['F', 'G', 'A', 'B', 'C', 'D', 'E']);
+      expect(getScale('Bb', 'lydian')).to.deep.eq(['Bb', 'C', 'D', 'E', 'F', 'G', 'A']);
+      // expect(getScale('C', 'lydian')).to.deep.eq(['C', 'D', 'E', 'F#', 'G', 'A', 'B']); // renders F# as Gb but should not
+    });
+
+    it('returns the correct scale for the mixolydian mode', () => {
+      expect(getScale('G', 'mixolydian')).to.deep.eq(['G', 'A', 'B', 'C', 'D', 'E', 'F']);
+      expect(getScale('C', 'mixolydian')).to.deep.eq(['C', 'D', 'E', 'F', 'G', 'A', 'Bb']);
+      expect(getScale('D', 'mixolydian')).to.deep.eq(['D', 'E', 'F#', 'G', 'A', 'B', 'C']);
+    });
 
     it('returns the correct scale for the aeolian (minor) mode', () => {
       expect(getScale('A', 'minor')).to.deep.eq(['A', 'B', 'C', 'D', 'E', 'F', 'G']);
-      // expect(getScale('D', 'minor')).to.deep.eq(['D', 'E', 'F', 'G', 'A', 'Bb', 'C']); // needs renders Bb as A# but should not
+      // expect(getScale('D', 'minor')).to.deep.eq(['D', 'E', 'F', 'G', 'A', 'Bb', 'C']); // renders Bb as A# but should not
       expect(getScale('E', 'minor')).to.deep.eq(['E', 'F#', 'G', 'A', 'B', 'C', 'D']);
+    });
+
+    it('returns the correct scale for the locrian mode', () => {
+      expect(getScale('B', 'locrian')).to.deep.eq(['B', 'C', 'D', 'E', 'F', 'G', 'A']);
+      // expect(getScale('E', 'locrian')).to.deep.eq(['E', 'F', 'G', 'A', 'Bb', 'C', 'D']); // renders Bb as A# but should not
+      // expect(getScale('F#', 'locrian')).to.deep.eq(['F#', 'G', 'A', 'B', 'C', 'D', 'E']); // does not recognize F# as a note
     });
   });
 });
